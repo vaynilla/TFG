4e281b184537138c70f24659e680ef06fd293adf
vaynilla <davidcr456@gmail.com>
2024-01-28 11:53:06 +0100

    App finalizada y memoria casi terminada

--- API_TFG/app.py
+++ API_TFG/app.py
@@ -1,28 +1,41 @@
 import traceback
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from scipy.integrate import solve_ivp
 import numpy as np
-import matplotlib.pyplot as plt
 from flask_cors import CORS
 
 app = Flask(__name__)
 
+# Para permitir el acceso desde la página web a la API
 CORS(app)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def inicio():  # put application's code here
     return jsonify({'name': 'Resolucion de sistemas de ecuaciones diferenciales'})
+
+
+@app.route('/swagger/<path:filename>')
+def serve_swagger(filename):
+    return send_from_directory('', filename)
 
 
 # Ruta para resolver sistemas de ecuaciones diferenciales
 @app.route('/resolver_ecuaciones', methods=['POST'])
 def resolver_ecuaciones_dif():
     data = request.get_json()  # Asumimos que los datos se van a recibir en formato JSON
-    # Comprobamos que tenemos ecuaciones y sus condiciones iniciales
+    # Comprobamos que tenemos ecuaciones, sus condiciones iniciales y la temperatura exterior
     if 'ecuaciones' in data and 'condiciones_iniciales' in data:
         ecuaciones = data['ecuaciones']
         condiciones_iniciales = data['condiciones_iniciales']
+        exterior = data['exterior']
+        # Creamos el array con la x de 0 a 24 horas
+        x = np.arange(0, 24, 0.1)
+
+        def obtener_temp_exterior(x):
+            return [eval(exterior, {"t": t, "np": np}) for t in x]
+
+        temp_exterior = obtener_temp_exterior(x)
 
         def fun(t, y):
             return [eval(ecuacion, {"y": y, "t": t, "np": np}) for ecuacion in ecuaciones]
@@ -35,15 +48,14 @@ def resolver_ecuaciones_dif():
             solucion = solve_ivp(fun, t_span, y0, t_eval=np.arange(0, 24, 0.1))
 
             # Devuelve la solución como JSON (una lista con los puntos en el tiempo y el valor en esos puntos)
-            resultado = {'t': solucion.t.tolist(), 'y': solucion.y.tolist()}
-            plt.plot(solucion.t, solucion.y.T)
-            plt.show()
+            resultado = {'t': solucion.t.tolist(), 'y': solucion.y.tolist(), 'temp_exterior': temp_exterior}
             # Devolvemos el resultado formado por el tiempo y la temperatura de cada habitación en cada instante de tiempo
             return jsonify(resultado), 201
 
         except Exception as e:
             # En caso de error, devuelve un mensaje de error
-            return jsonify({'error': str(e), 'linea_error': e.__traceback__.tb_lineno, 'traza_excepcion': traceback.format_exc()}), 400
+            return jsonify({'error': str(e), 'linea_error': e.__traceback__.tb_lineno,
+                            'traza_excepcion': traceback.format_exc()}), 400
 
     return jsonify(
         {'error': 'Se requieren ecuaciones diferenciales y condiciones iniciales para resolver el sistema.'}), 400
--- simulador/src/components/Formulario.jsx
+++ simulador/src/components/Formulario.jsx
@@ -4,9 +4,10 @@ import {Layout} from "antd";
 import {Content} from "antd/es/layout/layout";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import { Tabs } from 'antd';
 
 const Simulador = () => {
-    const { register, formState: { errors }, watch, handleSubmit, reset } = useForm();
+    const { register, formState: { errors }, watch, handleSubmit, reset} = useForm();
 
     const [datos, setDatos] = useState([]);
 
@@ -16,12 +17,16 @@ const Simulador = () => {
 
     // Función que se ejecutará cuando se envíe el formulario
     const onSubmit = async (data) => {
-        // reset();
+        // Si no hemos introducido la función, la creamos a partir de los otros datos
+        if (data.exterior === ""){
+            data.exterior = `${data.temp_promedio} - ${data.variacion}*np.cos(t*np.pi/12)`
+        }
 
         // Creamos el edificio a partir de los datos del formulario para enviarlo posteriormente a la API
         const edificio = {
             ecuaciones: [],
-            condiciones_iniciales: {}
+            condiciones_iniciales: {},
+            exterior: data.exterior
         };
 
         // Para cada habitación, añadimos en las condiciones iniciales un atributo y_i con la temperatura inicial de esa habitación
@@ -34,8 +39,10 @@ const Simulador = () => {
             // Comprobar tanto la cantidad de personas en la habitación como los aires acondicionados para luego meterlos en la ecuación
             let habitaciones_contiguas = data[`contiguas_${i}`].split(',');
             let ecuacion = "";
-            for (let j = 0; j < habitaciones_contiguas.length; j++){
-                ecuacion = ecuacion + `(y[${habitaciones_contiguas[j]}] - y[${i}])/${data.constante_habitaciones}+`;
+            if (num_habitaciones > 1) { // Si hay sólo 1 habitación, entonces no hay habitaciones contiguas y pasamos directamente al exterior
+                for (let j = 0; j < habitaciones_contiguas.length; j++) {
+                    ecuacion = ecuacion + `(y[${habitaciones_contiguas[j]}] - y[${i}])/${data.constante_habitaciones}+`;
+                }
             }
             // Ahora añadimos la parte con el exterior
             ecuacion = ecuacion + `(${data.exterior}-y[${i}])/${data.constante_exterior}`;
@@ -44,11 +51,10 @@ const Simulador = () => {
                 ecuacion = ecuacion + ` + (${data.fuerza_aire})`;
             }
             // Introducimos en la ecuación según la cantidad de personas
+            // eslint-disable-next-line no-useless-concat
             ecuacion = ecuacion + ` + ${data[`personas_${i}`]}` + `*${CALOR_HUMANO}`;
             edificio.ecuaciones.push(ecuacion);
         }
-
-        console.log(edificio);
 
         // Enviamos la información a la API
         try {
@@ -65,11 +71,14 @@ const Simulador = () => {
 
             // Añadimos al state los datos para poder utilizarlos luego en Highcharts
             setDatos(data)
-            console.log(datos)
+
         } catch (error) {
 
         }
+    }
 
+    const handleReset = () => {
+        reset();
     }
 
     // Función que nos permite modificar el formulario según el número de habitaciones y aumentar la cantidad de campos
@@ -100,21 +109,38 @@ const Simulador = () => {
 
     const convertir_datos = () => {
         // Para cuando todavía no hemos enviado el formulario
-        if (!datos || !datos.t || !Array.isArray(datos.y))
-            return []
-        else
-            return datos.y.map((habitacion, index) => ({
+        if (!datos || !datos.t || !Array.isArray(datos.y) || !datos.temp_exterior) {
+            return [];
+        } else {
+            // Incluir la temperatura exterior en los datos
+            const temperatura_exterior = datos.temp_exterior.map((temp, tIndex) => ({
+                x: datos.t[tIndex],
+                y: temp,
+            }));
+
+            // Mapear las habitaciones
+            const habitaciones = datos.y.map((habitacion, index) => ({
                 name: `Habitación ${index}`,
                 data: habitacion.map((temperatura, tIndex) => [datos.t[tIndex], temperatura]),
-            }))
-    }
+            }));
 
+            // Devolver un array que incluye las temperaturas de las habitaciones y la temperatura exterior
+            return [...habitaciones, { name: 'Temp. Exterior', data: temperatura_exterior }];
+        }
+    };
+
+    // Opciones de la gráfica en Highcharts
     const options = {
         chart: {
             type: 'line'
         },
         title: {
             text: 'Temperatura del edificio'
+        },
+        xAxis: {
+            title: {
+                text: 'Tiempo (Horas)'
+            }
         },
         yAxis: {
             title: {
@@ -132,15 +158,56 @@ const Simulador = () => {
         series: convertir_datos()
     }
 
+    // Para poner loos tabs de ant-design
+    const items = [
+        {
+            key: '1',
+            label: 'Opción 1',
+            children:
+                <div className="form-floating mb-2">
+                    <input type="text" className="form-control" placeholder="ejemplo" {...register('exterior', {   // Registramos el campo exterior en el formulario
+                        required: false  // Para que el campo sea rellenado obligatoriamente
+                    })}/>
+                    <label htmlFor="floatingInput">Función</label>
+                </div>
+        },
+        {
+            key: '2',
+            label: 'Opción 2',
+            children:
+                <div>
+                    <div className="form-floating mb-2">
+                        <input type="text" className="form-control" placeholder="ejemplo" {...register('temp_promedio', {
+                            required: false  // Para que el campo sea rellenado obligatoriamente
+                        })}/>
+                        <label htmlFor="floatingInput">Temperatura media</label>
+                    </div>
+                    <div className="form-floating mb-2">
+                        <input type="text" className="form-control" placeholder="ejemplo" {...register('variacion', {
+                            required: false  // Para que el campo sea rellenado obligatoriamente
+                        })}/>
+                        <label htmlFor="floatingInput">Variación de la temperatura</label>
+                    </div>
+                </div>
+        },
+    ];
+
+    const downloadTxtFile = () => {
+        // file object
+        const datosJSON = JSON.stringify(datos);
+        const file = new Blob([datosJSON], {type: 'text/plain'});
+        // anchor link
+        const element = document.createElement("a");
+        element.href = URL.createObjectURL(file);
+        element.download = "solucion.json";
+        // simulate link click
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+    }
+
     return <Layout hasSider><Content><div className={"container col-10 p-5"}>
         <div align="center"><h2>Generación del edificio</h2></div>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-floating mb-2">
-                <input type="text" className="form-control" placeholder="ejemplo" {...register('exterior', {   // Registramos el campo exterior en el formulario
-                    required: true  // Para que el campo sea rellenado obligatoriamente
-                })} required/>
-                <label htmlFor="floatingInput">Temperatura exterior</label>
-            </div>
             <div className="form-floating mb-2">
                 <input type={'number'} className="form-control" placeholder="ejemplo" {...register('num_habitaciones', {
                     required: true,
@@ -150,42 +217,115 @@ const Simulador = () => {
                 {errors.num_habitaciones?.type === 'validate'}
                 {num_habitaciones > MAX_HABITACIONES && <p>El edificio no puede tener más de {MAX_HABITACIONES} habitaciones</p>}
             </div>
-            {num_habitaciones <= MAX_HABITACIONES && addCampos("Temperatura inicial de la habitación ","y_",num_habitaciones, "number")}
-            {/*Mostramos el nivel de aislamiento entre habitaciones siempre que haya más de 1 habitación, de lo contrario no tendría sentido*/}
-            {num_habitaciones > 1 &&
-                <div className="form-floating mb-2">
-                    {/*Permitimos hasta 2 números decimales en la constante de aislamiento*/}
-                    <input type="number" step=".01" className="form-control" placeholder="ejemplo" {...register('constante_habitaciones', {
-                        required: true,
-                        validate: (value) => value > 0
-                    })} required/>
-                    <label htmlFor="floatingInput">Constante de transferencia entre habitaciones (A mayor número, menor aislamiento): </label>
-                    {errors.constante_habitaciones?.type === 'validate' && <p>Debe ser un número positivo</p>}
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+                            Temperatura exterior
+                        </button>
+                    </h2>
+                    <div id="collapseSix" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <Tabs defaultActiveKey="1" items={items} />
+                        </div>
+                    </div>
                 </div>
-            }
-            <div className="form-floating mb-2">
-                {/*Permitimos hasta 2 números decimales en la constante de aislamiento*/}
-                <input type="number" step=".01" className="form-control" placeholder="ejemplo" {...register('constante_exterior', {
-                    required: true,
-                    validate: (value) => value > 0
-                })} required/>
-                <label  htmlFor="floatingInput">Constante de transferencia del edificio con el exterior: </label>
-                {errors.constante_exterior?.type === 'validate' && <p>Debe ser un número positivo</p>}
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Temperaturas iniciales
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {num_habitaciones <= MAX_HABITACIONES && addCampos("Temperatura inicial de la habitación ","y_",num_habitaciones, "number")}
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                            Personas
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {num_habitaciones <= MAX_HABITACIONES && addCampos("Cantidad de personas en la habitación ","personas_",num_habitaciones, "number")}
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                            Aires acondicionados
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {num_habitaciones <= MAX_HABITACIONES && addCampos("Aire acondicionado en la habitación  ","aire_",num_habitaciones, "checkbox")}
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                            Distribución del edificio
+                        </button>
+                    </h2>
+                    <div id="collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {num_habitaciones <= MAX_HABITACIONES && num_habitaciones > 1 && addCampos("Habitaciones contiguas a la habitacion  ","contiguas_",num_habitaciones, "text")}
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                            Características del edificio
+                        </button>
+                    </h2>
+                    <div id="collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            {/*Mostramos el nivel de aislamiento entre habitaciones siempre que haya más de 1 habitación, de lo contrario no tendría sentido*/}
+                            {num_habitaciones > 1 &&
+                                <div className="form-floating mb-2">
+                                    {/*Permitimos hasta 2 números decimales en la constante de aislamiento*/}
+                                    <input type="number" step=".01" className="form-control" placeholder="ejemplo" {...register('constante_habitaciones', {
+                                        required: true,
+                                        validate: (value) => value > 0
+                                    })} required/>
+                                    <label htmlFor="floatingInput">Constante de transferencia entre habitaciones (A mayor número, mayor aislamiento): </label>
+                                    {errors.constante_habitaciones?.type === 'validate' && <p>Debe ser un número positivo</p>}
+                                </div>
+                            }
+                            <div className="form-floating mb-2">
+                                {/*Permitimos hasta 2 números decimales en la constante de aislamiento*/}
+                                <input type="number" step=".01" className="form-control" placeholder="ejemplo" {...register('constante_exterior', {
+                                    required: true,
+                                    validate: (value) => value > 0
+                                })} required/>
+                                <label  htmlFor="floatingInput">Constante de transferencia del edificio con el exterior: </label>
+                                {errors.constante_exterior?.type === 'validate' && <p>Debe ser un número positivo</p>}
+                            </div>
+                            <div className="form-floating mb-2">
+                                <input type='number' step={".001"} className="form-control" placeholder="ejemplo" {...register('fuerza_aire')}/>
+                                <label htmlFor="floatingInput">Calor generado por el aire acondicionado (ºC/h): </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {num_habitaciones <= MAX_HABITACIONES && addCampos("Cantidad de personas en la habitación ","personas_",num_habitaciones, "number")}
-            {num_habitaciones <= MAX_HABITACIONES && addCampos("Aire acondicionado en la habitación  ","aire_",num_habitaciones, "checkbox")}
-            {/*Maybe tengo que sacar este campo sólo si se ha marcado algún aire pero no se cómo hacerlo*/}
-            <div className="form-floating mb-2">
-                <input type='number' step={".001"} className="form-control" placeholder="ejemplo" {...register('fuerza_aire')} required/>
-                <label htmlFor="floatingInput">Fuerza del aire acondicionado (ºC/h): </label>
+            <div className="p-3" align={"center"}>
+                <input className="btn btn-primary" type="submit" value="Enviar" /> <button onClick={handleReset} className="btn btn-primary">Reset</button>
             </div>
-            {/*No controlamos que el usuario meta en el formato correcto las habitaciones contiguas*/}
-            {num_habitaciones <= MAX_HABITACIONES && addCampos("Habitaciones contiguas a la habitacion  ","contiguas_",num_habitaciones, "text")}
-            <div align={"center"}><input className="btn btn-primary" type="submit" value="Enviar" /></div>
         </form>
     </div></Content>
     <Content className="p-5">
+        <div className="" align={"center"}> <img src={"/LogoCS.png"} width={300} height={300} alt={""} /></div>
         <HighchartsReact highcharts={Highcharts} options={options}/>
+        <div className="p-3" align={"center"}>
+            <button id="downloadBtn" onClick={downloadTxtFile} className="btn btn-success" value="download">Descargar datos</button>
+        </div>
     </Content></Layout>
 }
 
